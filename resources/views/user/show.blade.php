117ea02ea3c67f0107de30b4e04fa1a3dc73c89a
董俊 <418826102@qq.com>
2019-10-07 13:03:39 +0800

    删除私信提交url

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -47,7 +47,8 @@
                                             <span>{{ $user->fan }}</span>
                                         </a>
                                     </li>
-                                    <li class="list-inline-item d-flex flex-column align-items-center" title="收到了{{ $user->zan }}个赞数">
+                                    <li class="list-inline-item d-flex flex-column align-items-center"
+                                        title="收到了{{ $user->zan }}个赞数">
                                         <span>点赞</span>
                                         <a href="">
                                             <span>{{ $user->zan }}</span>
@@ -88,7 +89,8 @@
                                 @if($user->website)
                                     <p title="网址">
                                         <i class="fa fa-link"></i>
-                                        <a href="{{ $user->website }}" target="_blank" class="text-muted">{{ $user->website }}</a>
+                                        <a href="{{ $user->website }}" target="_blank"
+                                           class="text-muted">{{ $user->website }}</a>
                                     </p>
                                 @endif
                                 <p title="最后在线">
@@ -108,8 +110,38 @@
                             <div class="btn-group">
                                 <a href="{{ route('users.follow', $user) }}"
                                    class="btn btn-success text-white">{{ $followStatus ? '取消关注' : '关注我' }}</a>
-                                <a href="#" class="btn btn-primary text-white" data-toggle="modal" data-target="#privateMessage">私信我</a>
+                                <a href="#" class="btn btn-primary text-white" data-toggle="modal"
+                                   data-target="#privateMessage">私信我</a>
                                 <a href="#" class="btn btn-danger text-white">打赏我</a>
+                            </div>
+
+                            {{--私信我--}}
+                            <div class="modal fade" id="privateMessage" tabindex="-1" role="dialog" aria-labelledby="privateMessageLabel"
+                                 aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <form action="" method="post">
+                                        @csrf
+                                        <input type="hidden" name="user_id" value="{{ $user->id }}">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="privateMessageLabel">私信我</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="form-group">
+                                <textarea class="form-control" name="private_message" rows="5"
+                                          placeholder="提示：技术问题请发问答帖，私信侵扰不礼貌。问答帖好处：1. 有助他人；2. 方便检索；3. 集思广益。也请勿回复此类私信。"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+                                                <button type="submit" class="btn btn-primary">发送</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         @endif
                     </div>
@@ -118,35 +150,6 @@
                 {{--最近访客--}}
                 @include('includes.visitor', ['user' => $user])
             </div>
-        </div>
-    </div>
-
-
-
-    {{--私信我--}}
-    <div class="modal fade" id="privateMessage" tabindex="-1" role="dialog" aria-labelledby="privateMessageLabel"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <form action="{{ route('users.doPrivateMessage', $user) }}" method="post">
-                @csrf
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="privateMessageLabel">私信我</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <textarea class="form-control" name="private_message" rows="5" placeholder="提示：技术问题请发问答帖，私信侵扰不礼貌。问答帖好处：1. 有助他人；2. 方便检索；3. 集思广益。也请勿回复此类私信。"></textarea>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
-                        <button type="submit" class="btn btn-primary">发送</button>
-                    </div>
-                </div>
-            </form>
         </div>
     </div>
 @stop
